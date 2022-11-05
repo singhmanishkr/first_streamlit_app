095920f2763a76fb729eeabbf0c185c5beef5bee
singhmanishkr <singh.manishkr@gmail.com>
2022-11-05 17:46:24 +0000

    Update streamlit_app.py
    Addition line about menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My parents new healthy dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega3 & Buleberry Oatmeal")
+streamlit.text("Kale, Spanich & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free Range Egg")
